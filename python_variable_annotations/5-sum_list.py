fea17caf7d914910fc33822bad1af28abba626a3
Yannick <yannicksookree@gmail.com>
2026-01-06 11:57:30 +0400

    upd

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Function that takes a list of floats and returns the sum of its float
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Function that returns the sum(float) of a list of floats
+
+    Args:
+    input_list: list[float]
+
+    Returns:
+    sum of input_list: float
+    """
+    return sum(input_list)
