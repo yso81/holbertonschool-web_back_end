2dbab4864154d1f1d9b9b5f6f9ead70e22c4731c
Yannick <yannicksookree@gmail.com>
2026-01-06 11:34:28 +0400

    upd

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Function that takes a float as argument and returns its string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes float and returns its string representation
+
+    Args:
+    n: float
+
+    Return:
+    str
+    """
+    return str(n)
