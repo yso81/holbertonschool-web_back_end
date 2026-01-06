837d46c85ea0d0cdac7c763f3f262f31a14e52d6
Yannick <yannicksookree@gmail.com>
2026-01-06 12:17:37 +0400

    upd

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ Function that takes a list of floats and returns the sum of its float
 """
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """
     Function that returns the sum(float) of a list of floats
