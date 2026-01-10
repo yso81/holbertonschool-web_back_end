720ac0c67a8017b589b6bee4f4a3cf2f3538cd23
Yannick <yannicksookree@gmail.com>
2026-01-10 12:24:39 +0400

    upd

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module executes multiple coroutines at the same time
+"""
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay for each wait_random call.
+
+    Returns:
+        List[float]: The list of all the delays in ascending order.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+
+    for future_tasks in asyncio.as_completed(tasks):
+        result = await future_tasks
+        delays.append(result)
+
+    return delays
