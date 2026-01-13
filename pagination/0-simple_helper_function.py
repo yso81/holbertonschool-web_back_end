3bf4f341cd345feeaf6af92bf6b278cafa8da63e
Yannick <yannicksookree@gmail.com>
2026-01-13 11:38:37 +0400

    upd

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -19,5 +19,5 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    
+
     return (start_index, end_index)
