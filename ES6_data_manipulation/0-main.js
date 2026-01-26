4de0246338650bfd7ee2e712e3f28a085dc9b758
Yannick <yannicksookree@gmail.com>
2026-01-26 10:59:38 +0400

    upd

--- ES6_data_manipulation/0-main.js
+++ ES6_data_manipulation/0-main.js
@@ -1,5 +1,3 @@
-[
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
+import getListStudents from "./0-get_list_students.js";
+
+console.log(getListStudents());
