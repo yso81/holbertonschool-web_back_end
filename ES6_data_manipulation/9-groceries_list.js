20e34ee82728e3080c8f34ba74ced2348586b8fe
Yannick <yannicksookree@gmail.com>
2026-01-29 13:56:18 +0400

    upd

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList(name, quantity) {
+    const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+  
+  return groceries;
+}
