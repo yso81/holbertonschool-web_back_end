3d56dd0b56063765ba591e161da98cad19432b09
Yannick <yannicksookree@gmail.com>
2026-04-22 14:01:26 +0400

    upd

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self,
+                        index: int = None,
+                        page_size: int = 10) -> Dict[int, list]:
+        assert isinstance(index, int) and index >= 0
+
+        data = []
+        self.indexed_data = self.indexed_dataset()
+        current_index = index
+
+        while len(data) < page_size and current_index < len(self.indexed_data):
+            item = self.indexed_data.get(current_index)
+            if item:
+                data.append(item)
+        current_index += 1
+
+        next_index = current_index if current_index < len(self.indexed_data) else None
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": len(data),
+            "next_index": next_index
+        }
