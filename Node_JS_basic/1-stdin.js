1938209a7725f6ed1b718e16f3114e0b664b5dc2
Yannick <yannicksookree@gmail.com>
2026-05-13 13:28:39 +0400

    upd

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.setEncoding('utf-8');
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
