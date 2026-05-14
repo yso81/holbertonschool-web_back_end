760c0f609aa0f8d9cb31d441ba0c60524a333bf3
Yannick <yannicksookree@gmail.com>
2026-05-14 14:25:32 +0400

    upd

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -5,32 +5,32 @@ function countStudents(path) {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-    if (lines.length === 0) {
-      throw new Error('Cannot load the database');
+    if (lines.length <= 1) {
+      console.log('Number of students: 0');
+      return;
     }
 
-    const students = lines.slice(1).map(
-        (line) => {
+    const students = lines.slice(1).map((line) => {
       const [firstname, , , field] = line.split(',');
       return { firstname, field };
     });
 
     console.log(`Number of students: ${students.length}`);
 
-    const fields = 
-    ({});
-    students.forEach(student => {
+    const fields = {};
+    students.forEach((student) => {
       if (!fields[student.field]) {
         fields[student.field] = [];
       }
       fields[student.field].push(student.firstname);
     });
 
-    for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
