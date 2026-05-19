753dbfe238a26c92b6a5f5cdfe9e2e0f1caebf9a
Yannick <yannicksookree@gmail.com>
2026-05-19 14:32:10 +0400

    upd

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -7,7 +7,7 @@ const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-if (req.url === '/') {
+  if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
@@ -15,13 +15,12 @@ if (req.url === '/') {
     try {
       const output = await countStudents(databaseFile);
       res.end(output);
-      
-      } catch (error) {
-        res.end(error.message);
-      }
-    } else {
-      res.statusCode = 404;
-      res.end('Not Found');
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
   }
 });
 
